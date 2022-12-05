25abcb125ecb8f6d86ec2338739b0cb1519d6dd6
kunalkeshan <kunalkeshan12@gmail.com>
2022-12-05 17:47:38 +0530

    chore: setting up cloudflare deployment

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,12 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+// Next.js Edge API Routes: https://nextjs.org/docs/api-routes/edge-api-routes
 
-type Data = {
-  name: string
-}
+import type { NextRequest } from 'next/server';
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export const config = { runtime: 'experimental-edge' };
+
+export default async function (req: NextRequest) {
+	return new Response(JSON.stringify({ name: 'John Doe' }), {
+		status: 200,
+		headers: { 'Content-Type': 'application/json' },
+	});
 }
