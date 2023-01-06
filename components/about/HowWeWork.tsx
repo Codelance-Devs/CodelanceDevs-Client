18bcd4211cf0812d5ddcf0e41e67f5c2aade3ee6
kunalkeshan <kunalkeshan12@gmail.com>
2023-01-07 03:29:30 +0530

    chore: links update and removed max width
    Co-authored-by: surendar pd <surendarpd007@gmail.com>

--- components/about/HowWeWork.tsx
+++ components/about/HowWeWork.tsx
@@ -17,7 +17,7 @@ const services = [
 			<PlanningIcon className='transition-all duration-300 group-hover:text-primary' />
 		),
 		description:
-			"Understanding and defining the project flow to establish the goals and vision of the project to ensure we capture all the requirements.",
+			'Understanding and defining the project flow to establish the goals and vision of the project to ensure we capture all the requirements.',
 	},
 	{
 		title: 'Design',
@@ -25,7 +25,7 @@ const services = [
 			<DesignIcon className='transition-all duration-300 group-hover:text-primary' />
 		),
 		description:
-			"After understanding your requirments, we will create layouts based on the MVP discussed. We use various tools to create and customize a prototype.",
+			'After understanding your requirments, we will create layouts based on the MVP discussed. We use various tools to create and customize a prototype.',
 	},
 	{
 		title: 'Development',
@@ -33,7 +33,7 @@ const services = [
 			<CodingIcon className='transition-all duration-300 group-hover:text-primary' />
 		),
 		description:
-		"We will start execution once all the metrics are defined. We ensure that the project schedules included with appropriate milestones.",
+			'We will start execution once all the metrics are defined. We ensure that the project schedules included with appropriate milestones.',
 	},
 	{
 		title: 'Launch',
@@ -56,20 +56,20 @@ const services = [
 const HowWeWork = () => {
 	return (
 		<>
-			<div
-				className='scroll-mt-24 grid px-3 py-16 md:px-5 lg:grid-cols-5 lg:px-[5vw] lg:py-[148px] w-full max-w-[1440px] mx-auto'
-			>
+			<div className='mx-auto grid w-full scroll-mt-24 px-3 py-16 md:px-5 lg:grid-cols-5 lg:px-[5vw] lg:py-[148px]'>
 				<div className='flex flex-col items-center justify-start p-[8px] text-center md:p-4 lg:col-span-5'>
 					<h1 className='mb-4 select-text text-[32px] font-medium text-[#23262f] md:text-[34px] lg:text-[40px]'>
 						How we work
 					</h1>
 					<p className='text-[18px] text-[#4c4f58]'>
-					We follow the most effective working methodology. We believe success always comes when we have creativity, efforts, determination in our team.
+						We follow the most effective working methodology. We
+						believe success always comes when we have creativity,
+						efforts, determination in our team.
 					</p>
 				</div>
 				{services.map((service, idx) => (
 					<div key={idx} className='w-full px-[8px] py-4 md:p-1 '>
-						<div className='group flex flex-col items-center h-full w-full select-none rounded-3xl px-[24px] py-10 md:px-4 md:py-14'>
+						<div className='group flex h-full w-full select-none flex-col items-center rounded-3xl px-[24px] py-10 md:px-4 md:py-14'>
 							<div className='mb-4 flex h-16 w-16 items-center justify-center rounded-2xl bg-white shadow-md'>
 								{service.icon}
 							</div>
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,18 +1,18 @@
 import { NextPage } from 'next';
 import Head from 'next/head';
-import React from 'react'
+import React from 'react';
 import HowWeWork from '../components/about/HowWeWork';
 import PublicLayout from '../layouts/public';
 
-const About:NextPage = () => {
-    return (
-        <PublicLayout>
-            <Head>
-                <title>About</title>
-            </Head>
-            <HowWeWork/>
-        </PublicLayout>
-    )
-}
+const About: NextPage = () => {
+	return (
+		<PublicLayout>
+			<Head>
+				<title>About</title>
+			</Head>
+			<HowWeWork />
+		</PublicLayout>
+	);
+};
 
-export default About
+export default About;
