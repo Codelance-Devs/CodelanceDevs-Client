aebab63b9112ddf1ee673bab67d654b224d21b7c
Kunal Keshan <kunalkeshan12@gmail.com>
2023-01-16 22:47:20 +0530

    fix: added api link directly

--- config/index.ts
+++ config/index.ts
@@ -7,5 +7,5 @@
 export const isProduction = process.env.NODE_ENV === 'production';
 
 export const CODELANCE_DEVS_SERVER_BASE_URL = isProduction
-	? process.env.CODELANCE_DEVS_SERVER_BASE_URL
+	? 'https://api.codelancedevs.com'
 	: `http://localhost:5000`;
